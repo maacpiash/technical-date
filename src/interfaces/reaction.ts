b8e089cd903067e05f59f6a4f35c43752e0ceac3
Ahad Chowdhury <ahad@maacpiash.com>
2022-03-31 18:11:56 +1100

    👔 Add entities `Reaction` and `ReactionCount

--- src/interfaces/reaction.ts
+++ src/interfaces/reaction.ts
@@ -15,8 +15,26 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-export type { Post } from './post'
-export type { Author } from './author'
-export type { Comment } from './comment'
-export type { Reader } from './reader'
-export type { Reaction, ReactionCount } from './reaction'
+import { Reader } from './reader'
+
+export type Reaction =
+  | '👍'
+  | '👎'
+  | '😲'
+  | '😄'
+  | '❤️'
+  | '😞'
+  | '🚀'
+  | '🎉'
+  | '👀'
+  | '🤦'
+  | '😕'
+  | '😠'
+  | '🤝'
+  | '🤔'
+  | '🤐'
+  | '🤨'
+
+export type ReactionCount = {
+  [reaction in Reaction]: Reader[]
+}
